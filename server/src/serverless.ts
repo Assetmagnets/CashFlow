2cf497d23c8ac826af50b9ee38421e52a14570d3
Assetmagnets <assetmagnets@gmail.com>
2026-05-25 17:31:19 +0530

    fix: clear unused serverless.ts helper file to resolve NestJS compiler errors

--- server/src/serverless.ts
+++ server/src/serverless.ts
@@ -1,4 +1,1 @@
-// Serverless entry point - exports AppModule and utilities for the Vercel handler
-export { AppModule } from './src/app.module';
-export { HttpExceptionFilter } from './src/common/filters/http-exception.filter';
-export { ResponseInterceptor } from './src/common/interceptors/response.interceptor';
+// Deprecated: No longer needed. We import directly from dist/src/app.module in our JS handler.
